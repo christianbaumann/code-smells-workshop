23c649ca2f1bc223b0437374e69b26aef916a162
Chriss <chriss713@gmail.com>
2022-09-15 16:48:47 +0200

    Obfuscated variable names

--- src/main/java/dev/christianbaumann/Password.java
+++ src/main/java/dev/christianbaumann/Password.java
@@ -5,21 +5,23 @@ import java.util.regex.Pattern;
 
 public class Password {
 
-    public Boolean validatePasswordStrength(String password) {
-        if (!password.isEmpty()) {
-            Pattern p = Pattern.compile("\\d", Pattern.CASE_INSENSITIVE);
-            Matcher m = p.matcher(password);
-            if (m.find()) {
-                if (password.length() > 10) {
-                    return true;
-                } else {
-                    return false;
-                }
+public Boolean validatePasswordStrength(String p) {
+    if (!p.isEmpty()) {
+        Pattern pa = Pattern.compile("\\d", Pattern.CASE_INSENSITIVE);
+        Matcher m = pa.matcher(p);
+        if (m.find()) {
+            if (p.length() > 10) {
+                return true;
             } else {
                 return false;
             }
         } else {
             return false;
         }
+    } else {
+        return false;
     }
 }
+
+}
+
