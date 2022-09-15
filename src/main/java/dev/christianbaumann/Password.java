d78cceb633a9030dc82a987396760438eccd1290
Chriss <chriss713@gmail.com>
2022-09-15 17:13:35 +0200

    Removed magic number for minimum length

--- src/main/java/dev/christianbaumann/Password.java
+++ src/main/java/dev/christianbaumann/Password.java
@@ -5,12 +5,14 @@ import java.util.regex.Pattern;
 
 public class Password {
 
+    private static final Integer passwordMinimumLength = 11;
+
 public Boolean isPasswordValid(String password) {
     if (!password.isEmpty()) {
         Pattern pattern = Pattern.compile("\\d", Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(password);
         if (matcher.find()) {
-            if (password.length() > 10) {
+            if (password.length() >= passwordMinimumLength) {
                 return true;
             } else {
                 return false;
