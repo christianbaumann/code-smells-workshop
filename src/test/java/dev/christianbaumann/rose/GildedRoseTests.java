6c3ad6135a8c3b01a854269a5c4e3a014e1350e1
Chriss <chriss713@gmail.com>
2022-09-16 10:52:10 +0200

    Added gilded rose

---
+++ src/test/java/dev/christianbaumann/rose/GildedRoseTests.java
@@ -0,0 +1,16 @@
+package dev.christianbaumann.rose;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GildedRoseTests {
+
+    @Test
+    public void foo() {
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("fixme", app.items[0].name);
+    }
+}
