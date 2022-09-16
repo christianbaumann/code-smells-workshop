32ac7810d5726c74527d5bd90b3ca5d094fc65e5
Chriss <chriss713@gmail.com>
2022-09-16 09:04:52 +0200

    Removed magic number

--- src/main/java/dev/christianbaumann/Person.java
+++ src/main/java/dev/christianbaumann/Person.java
@@ -2,8 +2,11 @@ package dev.christianbaumann;
 
 public class Person {
 
+
+    private static final int AGE_TO_BUY_ALCOHOL = 16;
+
     public Boolean isOldEnoughToBuyAlcohol(int age) {
-        if (age >= 16) {
+        if (age >= AGE_TO_BUY_ALCOHOL) {
             return true;
         } else {
             return false;
