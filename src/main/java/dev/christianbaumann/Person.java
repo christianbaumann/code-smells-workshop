4349ff628a52b50c5eb3b0347fd503b486a668bd
Chriss <chriss713@gmail.com>
2022-09-16 09:05:43 +0200

    Removed if-else

--- src/main/java/dev/christianbaumann/Person.java
+++ src/main/java/dev/christianbaumann/Person.java
@@ -6,11 +6,7 @@ public class Person {
     private static final int AGE_TO_BUY_ALCOHOL = 16;
 
     public Boolean isOldEnoughToBuyAlcohol(int age) {
-        if (age >= AGE_TO_BUY_ALCOHOL) {
-            return true;
-        } else {
-            return false;
-        }
+        return age >= AGE_TO_BUY_ALCOHOL;
     }
 }
 
