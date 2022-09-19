0396963173ac208feddae28d40824609fa9c4dcd
Chriss <chriss713@gmail.com>
2022-09-19 07:39:54 +0200

    Optimized imports

--- src/test/java/dev/christianbaumann/password/PasswordTests.java
+++ src/test/java/dev/christianbaumann/password/PasswordTests.java
@@ -1,10 +1,9 @@
 package dev.christianbaumann.password;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
-
-import dev.christianbaumann.password.Password;
 import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class PasswordTests {
 
--- src/test/java/dev/christianbaumann/person/PersonTests.java
+++ src/test/java/dev/christianbaumann/person/PersonTests.java
@@ -2,8 +2,8 @@ package dev.christianbaumann.person;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class PersonTests {
 
