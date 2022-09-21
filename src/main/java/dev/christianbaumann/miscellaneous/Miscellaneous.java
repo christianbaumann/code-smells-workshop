a4e9e6f356d7b21d32c858713e66e98f3d5c2af8
Chriss <chriss713@gmail.com>
2022-09-21 14:48:20 +0200

    Added naming smells

---
+++ src/main/java/dev/christianbaumann/miscellaneous/Miscellaneous.java
@@ -0,0 +1,14 @@
+package dev.christianbaumann.miscellaneous;
+
+public class Miscellaneous {
+
+    String foo;
+    Integer bar;
+
+    String object;
+    Integer data;
+
+    Object acc;
+    Object poc;
+
+}
